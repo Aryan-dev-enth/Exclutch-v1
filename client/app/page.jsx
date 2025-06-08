f10f8e56a3ab889b4251881706569d01b0a76e9b
Aryan Singh <aryan28november@gmail.com>
2025-06-08 18:30:18 +0530

    updateted few changes

--- client/app/page.jsx
+++ client/app/page.jsx
@@ -25,7 +25,7 @@ export default function Home() {
     if (!user) {
       const timer = setTimeout(() => {
         setShowAuthPopup(true);
-      }, 1500);
+      }, 200);
       return () => clearTimeout(timer);
     }
   }, [user]);
